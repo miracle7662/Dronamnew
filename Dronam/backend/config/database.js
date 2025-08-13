0610ebda8eb010f30613440c2e11399ead06bab2
miracle7662 <82318479+miracle7662@users.noreply.github.com>
2025-08-13 18:01:19 +0530

    Update database.js
    df

--- Dronam/backend/config/database.js
+++ Dronam/backend/config/database.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'Sudarshan@7234',
+  password: process.env.MYSQL_PASSWORD || '',
   database: process.env.MYSQL_DATABASE || 'dronam',
   port: process.env.MYSQL_PORT || 3306,
   waitForConnections: true,
